37aebc44665b2f23683600d20fc6e31a071176e1
AnatoliiStepanovLikhoi <AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-04-11 19:40:32 +0000

    deploy: eb137a011e7f9c5a58a81af2de6357566cd2bae2

---
+++ assets/App.styled-82349799.js
@@ -0,0 +1,25 @@
+import{n as t}from"./index-1454a377.js";const a=t.div`
+  display: flex;
+  flex-direction: column;
+
+  /* box-shadow: rgb(182 182 182) 2px 10px 20px; */
+  width: 100%;
+  padding: ${e=>e.theme.space[4]};
+  text-align: center;
+
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: ${e=>e.theme.space[6]};
+`,i=t.h1`
+  font-size: ${e=>e.theme.fontsSizes.xl}px;
+  padding-bottom: ${e=>e.theme.space[3]};
+
+  /* color: ${e=>e.theme.colors.bgGrey}; */
+`,n=t.h2`
+  font-size: ${e=>e.theme.fontsSizes.l}px;
+  padding-bottom: ${e=>e.theme.space[4]};
+`,s=t.img`
+  width: 480px;
+  margin-left: auto;
+  margin-right: auto;
+`;export{a as C,s as H,i as M,n as S};
